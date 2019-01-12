b6e08e3a41e07c7a31ee5ed051dcbefe4f98d921
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-01-12 11:05:48 +0530

    Conditionals

--- public/app.js
+++ public/app.js
@@ -1,43 +1,16 @@
 new Vue({
   el: "#app",
   data: {
-    name: "Doctor",
-    x: 0,
-    y: 0,
-    message: '',
-    red: true,
-    blue: false,
+    error: false,
+    success: false,
   },
   methods: {
-    greet: function(time){
-      return "Hello " + this.name;
-    },
-    changeName: function(){
-      this.name = "River Song";
-    },
-    changeTitle: function(){
-      this.name = "Doctor"
-    },
-    move: function(event){
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    clicked: function(event){
-      this.name = "Amelia Pond";
-    },
-    changeColor: function(){
-      this.red = !this.red;
-      this.blue = !this.blue;
-    }
+
   },
   computed: {
-    reverseName: function(){
-      return this.name.split('').reverse().join('');
-    },
+
   },
   watch: {
-    name: function(){
-      this.message = "Name is changing to "+this.name;
-    },
+
   },
 });
