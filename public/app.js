23be2bfa60d7d0a8dcd98baf232a87efea95f402
devakrishna33 <36110228+devakrishna33@users.noreply.github.com>
2019-01-12 11:24:18 +0530

    Loops

--- public/app.js
+++ public/app.js
@@ -1,8 +1,19 @@
 new Vue({
   el: "#app",
   data: {
-    error: false,
-    success: false,
+    doctors: [{
+      name: "Joddie Whittaker",
+      number: 13,
+    }, {
+      name: "Peter Capaldi",
+      number: 12,
+    }, {
+      name: "Matt Smith",
+      number: 11,
+    }, {
+      name: "David Tennant",
+      number: 10,
+    }],
   },
   methods: {
 
